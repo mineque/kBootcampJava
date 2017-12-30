d713920e58a5927c4577c6393a833dca680cc955
mineque <mineque@quanteam.pl>
2017-12-30 01:39:25 +0100

    Changes for mod6.2

--- kodilla-testing/src/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,37 @@
 package com.kodilla.testing;
 
-public class TestingMain {
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
-    public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+public class TestingMain {
+    public static void main(String[] args){
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calc = new Calculator();
+
+        int resultA = calc.add(2,3);
+
+        if( resultA == 5) {
+            System.out.println("Result OK");
+        } else {
+            System.out.println("Something went wrong!");
+        }
+
+        int resultB = calc.substract(4,3);
+
+        if( resultB == -1) {
+            System.out.println("Result OK");
+        } else {
+            System.out.println("Something went wrong!");
+        }
+
     }
 }
---
+++ kodilla-testing/src/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int a;
+    int b;
+
+    public Calculator() {
+    }
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int substract(int a, int b) {
+        return a - b;
+    }
+
+}
