f76cb9a4466d01527a5bad3eebd0ae7f320fbb55
mineque <mineque@quanteam.pl>
2017-12-30 22:42:44 +0100

    6.2

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -1,9 +1,12 @@
 package com.kodilla.testing.forum;
 
 import com.kodilla.testing.user.SimpleUser;
+
 import org.junit.Assert;
 import org.junit.Test;
+
 public class ForumTestSuite {
+
     @Test
     public void testCaseUsername(){
         //Given
