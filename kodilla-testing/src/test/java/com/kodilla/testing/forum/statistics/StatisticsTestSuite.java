58477d9fa710bc6c8f2a79f1f07d4398597aef6c
mineque <mineque@quanteam.pl>
2018-01-02 01:45:45 +0100

    Mod6.5 Zadanie

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
@@ -12,7 +12,7 @@ import static org.mockito.Mockito.when;
 public class StatisticsTestSuite {
 
     @Test
-    public void testAverageWhenZero() {
+    public void testAverage0() {
         //Given
         Statistics statisticsMock = mock(Statistics.class);
 
@@ -34,7 +34,7 @@ public class StatisticsTestSuite {
     }
 
     @Test
-    public void testAverageWhen0_10() {
+    public void testAverageWhen1() {
         //Given
         Statistics statisticsMock = mock(Statistics.class);
 
@@ -58,5 +58,96 @@ public class StatisticsTestSuite {
         Assert.assertEquals(10, stats.getAvgCommentPerPost(), 1);
         Assert.assertEquals(20, stats.getAvgCommentPerUser(), 1);
         Assert.assertEquals(2, stats.getAvgPostPerUser(), 1);
+    }
+
+    @Test
+    public void testAverage2() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+
+        List<String> usersList = new ArrayList<>();
+        usersList.add("John Doe");
+        usersList.add("Doe John");
+        usersList.add("John Smith");
+        usersList.add("Smith John");
+        usersList.add("Jane Doe");
+
+        when(statisticsMock.usersNames()).thenReturn(usersList);
+        when(statisticsMock.commentsCount()).thenReturn(10);
+        when(statisticsMock.postsCount()).thenReturn(15);
+
+        UserStats stats = new UserStats();
+        //When
+
+        stats.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        Assert.assertEquals(.66, stats.getAvgCommentPerPost(), 0.01);
+        Assert.assertEquals(2, stats.getAvgCommentPerUser(), 0.01);
+        Assert.assertEquals(3, stats.getAvgPostPerUser(), 0.01);
+    }
+    @Test
+    public void testAverage3() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+
+        List<String> usersList = new ArrayList<>();
+        usersList.add("John Doe");
+        usersList.add("Doe John");
+        usersList.add("John Smith");
+        usersList.add("Smith John");
+        usersList.add("Jane Doe");
+        usersList.add("Doe John");
+        usersList.add("John Smith");
+        usersList.add("Smith John");
+        usersList.add("Jane Doe");
+        usersList.add("Doe John");
+        usersList.add("John Smith");
+        usersList.add("Smith John");
+        usersList.add("Jane Doe");
+
+        when(statisticsMock.usersNames()).thenReturn(usersList);
+        when(statisticsMock.commentsCount()).thenReturn(1);
+        when(statisticsMock.postsCount()).thenReturn(15);
+
+        UserStats stats = new UserStats();
+        //When
+
+        stats.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        Assert.assertEquals(.06, stats.getAvgCommentPerPost(), 0.01);
+        Assert.assertEquals(.07, stats.getAvgCommentPerUser(), 0.01);
+        Assert.assertEquals(1.16, stats.getAvgPostPerUser(), 0.01);
+    }
+    @Test
+    public void testAverage4() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+
+        List<String> usersList = new ArrayList<>();
+        usersList.add("John Doe");
+        usersList.add("Doe John");
+        usersList.add("John Smith");
+        usersList.add("Smith John");
+        usersList.add("Jane Doe");
+        usersList.add("Doe John");
+        usersList.add("John Smith");
+        usersList.add("Smith John");
+        usersList.add("Jane Doe");
+
+        when(statisticsMock.usersNames()).thenReturn(usersList);
+        when(statisticsMock.commentsCount()).thenReturn(0);
+        when(statisticsMock.postsCount()).thenReturn(100);
+
+        UserStats stats = new UserStats();
+        //When
+
+        stats.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        Assert.assertEquals(0, stats.getAvgCommentPerPost(), 0.01);
+        Assert.assertEquals(0, stats.getAvgCommentPerUser(), 0.01);
+        Assert.assertEquals(11.12, stats.getAvgPostPerUser(), 0.01);
     }
 }
