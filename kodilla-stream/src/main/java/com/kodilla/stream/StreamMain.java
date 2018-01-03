dddeec22723ce3809f9d237faca70190ae39a857
mineque <mineque@quanteam.pl>
2018-01-04 00:36:36 +0100

    Mod7.1 Zadanie

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.FunctionalCalculator;
@@ -21,5 +22,14 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemo = new PoemBeautifier();
+
+        poemo.beautify("bla bla", (text) -> text.toUpperCase());
+        poemo.beautify("bla bla", (text) -> text.toUpperCase());
+        poemo.beautify("BLA BLA", (text) -> "aaaa "+text+" bbb");
+        poemo.beautify("BLA BLA", (text) -> text.toLowerCase());
+
+
     }
 }
