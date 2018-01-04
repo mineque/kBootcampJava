df213920af3927e6694e3f1e446055368e062b87
mineque <mineque@quanteam.pl>
2018-01-04 22:46:06 +0100

    Mod7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,10 +1,11 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.person.People;
+import com.kodilla.stream.book.BookDirectory;
 
 public class StreamMain {
     public static void main(String[] args) {
-      /*  Processor processor = new Processor();
+/*
+        Processor processor = new Processor();
         processor.execute(() -> System.out.println("This is an example text."));
 
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
@@ -28,13 +29,18 @@ public class StreamMain {
         poemo.beautify("BLA BLA", (text) -> text.toLowerCase());
 
         System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);*/
+        NumbersGenerator.generateEven(20);
 
         People.getList().stream()
                 .map(String::toUpperCase)
                 .filter(s -> s.length() > 11)
                 .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
                 .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
+*/
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
                 .forEach(System.out::println);
     }
 }
