551b9d306238c8cc141ecfad7eca4c9afc02efb8
mineque <mineque@quanteam.pl>
2018-01-04 23:03:32 +0100

    Mod7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,7 +3,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
 
-import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
@@ -43,12 +43,14 @@ public class StreamMain {
                 .forEach(System.out::println);
 */
         BookDirectory theBookDirectory = new BookDirectory();
-        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .collect(Collectors.toList());
 
-        System.out.println("# elements: " + theResultListOfBooks.size());
-        theResultListOfBooks.stream()
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
     }
 }
