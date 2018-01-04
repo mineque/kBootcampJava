8ae7f0f4e34dfc5b462c064dbb35dfd9b198aac0
mineque <mineque@quanteam.pl>
2018-01-04 23:38:55 +0100

    Mod7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,54 +3,16 @@ package com.kodilla.stream;
 import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
 
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-/*
-        Processor processor = new Processor();
-        processor.execute(() -> System.out.println("This is an example text."));
-
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-        PoemBeautifier poemo = new PoemBeautifier();
-
-        poemo.beautify("bla bla", (text) -> text.toUpperCase());
-        poemo.beautify("bla bla", (text) -> text.toUpperCase());
-        poemo.beautify("BLA BLA", (text) -> "aaaa "+text+" bbb");
-        poemo.beautify("BLA BLA", (text) -> text.toLowerCase());
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
-
-        People.getList().stream()
-                .map(String::toUpperCase)
-                .filter(s -> s.length() > 11)
-                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
-                .filter(s -> s.substring(0, 1).equals("M"))
-                .forEach(System.out::println);
-*/
         BookDirectory theBookDirectory = new BookDirectory();
-
-        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
                 .filter(book -> book.getYearOfPublication() > 2005)
-                .collect(Collectors.toMap(Book::getSignature, book -> book));
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
 
-        System.out.println("# elements: " + theResultMapOfBooks.size());
-        theResultMapOfBooks.entrySet().stream()
-                .map(entry -> entry.getKey() + ": " + entry.getValue())
-                .forEach(System.out::println);
+        System.out.println(theResultStringOfBooks);
     }
 }
