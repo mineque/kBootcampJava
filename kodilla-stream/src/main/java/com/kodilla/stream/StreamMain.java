3b64781878613744325bd072c1aa76a6bdba171a
mineque <mineque@quanteam.pl>
2018-01-04 22:41:34 +0100

    Mod7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -31,7 +31,10 @@ public class StreamMain {
         NumbersGenerator.generateEven(20);*/
 
         People.getList().stream()
+                .map(String::toUpperCase)
                 .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
                 .forEach(System.out::println);
     }
 }
