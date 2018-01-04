e51e36f0909dfbd8336e82ae8922d93f33ae7fa4
mineque <mineque@quanteam.pl>
2018-01-04 22:04:28 +0100

    Mod7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.FunctionalCalculator;
@@ -30,6 +31,8 @@ public class StreamMain {
         poemo.beautify("BLA BLA", (text) -> "aaaa "+text+" bbb");
         poemo.beautify("BLA BLA", (text) -> text.toLowerCase());
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
