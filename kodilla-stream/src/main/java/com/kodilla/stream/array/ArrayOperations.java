3dac13be6f0a2cfaacfd08c939441b9c56a7dae1
mineque <mineque@quanteam.pl>
2018-01-21 20:37:00 +0100

    Mod7.5 Zadanie

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,33 +1,25 @@
 package com.kodilla.stream.array;
 
+import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
 
-    static double getAverage(int[] numbers){
+    static double getAverage(int[] numbers) {
 
         int[] numbersArray = numbers;
 
-        IntStream display = IntStream.range(0,numbersArray.length)
-                display.forEach(System.out::println);
-                double result = display.average(numbersArray[n]);
+        IntStream.range(0, numbersArray.length)
+                .map(n -> numbersArray[n])
+                .forEach(System.out::println);
 
-//        numbersArray.IntStream.range(0,numbersArray.length())
-//                .
-//                .forEach(System.out::println);
-//                .
-//    }
-//    long numberOfBooksPublicatedAfter2007 = IntStream.range(0, books.size())
-//            .filter(n -> books.get(n).getYearOfPublication() > 2007)
-//            .count();
-//        Assert.assertEquals(3, numberOfBooksPublicatedAfter2007);
-    //
-//    public BigDecimal getPeopleQuantity(){
-//        //
-//        return continents.stream()
-//                .flatMap(c -> c.getCountries().stream())
-//                .map(cnt -> cnt.getPeopleQuantity())
-//                .reduce(BigDecimal.ZERO,(sum, delta) -> sum.add(delta));
+        OptionalDouble average = IntStream.range(0, numbersArray.length)
+                .map(n -> numbersArray[n])
+                .average();
+
+        double value = average.orElse(0);
+        return value;
+    }
 
 
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayTestSuite.java
@@ -1,4 +1,19 @@
 package com.kodilla.stream.array;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ArrayTestSuite {
+@Test
+    public void PrintTest() {
+
+        //Given
+        int[] numbers = {1,3,5,1,10,21};
+
+        //When
+        double avg = ArrayOperations.getAverage(numbers);
+
+        //Then
+        Assert.assertEquals(6.83, avg,1);
+    }
 }
