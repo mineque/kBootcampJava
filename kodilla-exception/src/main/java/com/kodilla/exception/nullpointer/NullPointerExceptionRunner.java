d9f6dd9aafadf38c4d63443dadd59cd9a8363dd6
mineque <mineque@quanteam.pl>
2018-01-26 22:18:45 +0100

    Mod8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/nullpointer/NullPointerExceptionRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/nullpointer/NullPointerExceptionRunner.java
@@ -2,10 +2,16 @@ package com.kodilla.exception.nullpointer;
 
 public class NullPointerExceptionRunner {
     public static void main(String[] args) {
+        //User user = new User("Josiek");
         User user = null;
-
         MessageSender messageSender = new MessageSender();
-        messageSender.sendMessageTo(user, "Hello!");
+        try {
+            messageSender.sendMessageTo(user, "message");
+        } catch (MessageNotSentException e) {
+            System.out.println("Message is not send," +
+                    "but my program still running very well!");
+        }
 
+        System.out.println("Processing other logic!");
     }
 }
