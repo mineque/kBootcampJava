175f03f36cb72d9cfe008d730a30add4da489795
mineque <mineque@quanteam.pl>
2018-01-27 23:33:51 +0100

    Mod10.1

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTests.java
@@ -0,0 +1,15 @@
+package com.kodilla.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRunnerTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
