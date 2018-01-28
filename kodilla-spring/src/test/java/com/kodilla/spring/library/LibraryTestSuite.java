2eb4763985ec80c34e2c230f033c73c41f0c6ee2
mineque <mineque@quanteam.pl>
2018-01-28 01:07:55 +0100

    Mod10.3

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -1,26 +1,28 @@
-package com.kodilla.spring.library;
+    package com.kodilla.spring.library;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
+    import org.junit.Test;
+    import org.junit.runner.RunWith;
+    import org.springframework.boot.test.context.SpringBootTest;
+    import org.springframework.context.ApplicationContext;
+    import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+    import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class LibraryTestSuite {
-    @Test
-    public void testLoadFromDb() {
-        //Given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Library library = context.getBean(Library.class);
-        //When
-        library.loadFromDb();
-        //Then
-        //do nothing
-    }
+    import java.util.Arrays;
+
+    @RunWith(SpringRunner.class)
+    @SpringBootTest
+    public class LibraryTestSuite {
+        @Test
+        public void testLoadFromDb() {
+            //Given
+            ApplicationContext context =
+                    new AnnotationConfigApplicationContext("com.kodilla.spring");
+            Library library = context.getBean(Library.class);
+            //When
+            library.loadFromDb();
+            //Then
+            //do nothing
+        }
 
     @Test
     public void testSaveToDb() {
@@ -33,4 +35,16 @@ public class LibraryTestSuite {
         //Then
         //do nothing
     }
+        @Test
+        public void testContext() {
+            //Given
+            ApplicationContext context =
+                    new AnnotationConfigApplicationContext("com.kodilla.spring.library");
+
+            //When & Then
+            System.out.println("===== Beans list: ==== >>");
+            Arrays.stream(context.getBeanDefinitionNames())
+                    .forEach(System.out::println);
+            System.out.println("<< ===== Beans list ====");
+        }
 }
