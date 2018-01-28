c4e056eceba86a9fc88012d111c127ff5767e2cb
mineque <mineque@quanteam.pl>
2018-01-28 11:34:11 +0100

    Mod10.4 Zadanie: Wszystko w jednym

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -18,14 +18,16 @@ public class BoardTestSuite {
         Board board = context.getBean(Board.class);
         //When
 
-        board.doneList.addTask("Zadanko zakonczone");
+        board.doneList.addTask("Zadanko nr1");
        // board.toDoList.addTask("Zadanko do zrobienia");
-        board.inProgressList.addTask("Zadanko w trakcie");
+        board.inProgressList.addTask("Zadanko ciężkie");
 
         //Then
 
-        System.out.println("Wykonano: " + board.doneList.getTasks().size());
+        System.out.println("Wykonano zadań: " + board.doneList.getTasks().size());
+        System.out.println("Wykonano zadanie: " + board.doneList.getTasks().get(0));
         System.out.println("Do zrobienia: " + board.toDoList.getTasks().size());
-        System.out.println("W trakcie: " + board.inProgressList.getTasks().size());
+        System.out.println("W trakcie zadań: " + board.inProgressList.getTasks().size());
+        System.out.println("W trakcie zadania: " + board.inProgressList.getTasks().get(0));
     }
 }
