5bc16d7dcbfbea063a1ff72b23a275e4866d9f39
mineque <mineque@quanteam.pl>
2018-01-28 01:34:10 +0100

    Mod10.3 Zadanie: Tablica z zadaniami

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -0,0 +1,16 @@
+package com.kodilla.spring.portfolio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private List<String> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(String s){
+        this.tasks.add(s);
+    }
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoardTestSuite {
+    @Test
+    public void testTaskAdd(){
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Board board = context.getBean(Board.class);
+        //When
+
+        board.doneList.addTask("Zadanko zakonczone");
+        board.toDoList.addTask("Zadanko do zrobienia");
+        board.inProgressList.addTask("Zadanko w trakcie");
+
+        //Then
+
+        TaskList<String> list = board.doneList;
+
+    }
+}
