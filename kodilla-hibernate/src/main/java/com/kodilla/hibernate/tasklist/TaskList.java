aefc5748f897fa552d3234857539d1a868b9ee85
mineque <mineque@quanteam.pl>
2018-02-04 23:00:23 +0100

    Mod13.3 Hibernate 1:1, 1:N

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,16 +1,21 @@
 package com.kodilla.hibernate.tasklist;
 
 
+import com.kodilla.hibernate.task.Task;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table (name = "TASKLISTS")
 public class TaskList {
 
-    int id;
-    String listName;
-    String description;
+    private int id;
+    private String listName;
+    private String description;
+    private List<Task>  tasks = new ArrayList<>();
 
     public TaskList(String listName) {
         this.listName = listName;
@@ -46,5 +51,21 @@ public class TaskList {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+
+
+    @OneToMany (
+            targetEntity = Task.class,
+            mappedBy =  "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
